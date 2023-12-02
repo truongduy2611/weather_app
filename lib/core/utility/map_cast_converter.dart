7393a73599a877afa48591a701d0e7e4f6b113fc
Duy Nguyen <truongduy.2611@gmail.com>
2023-12-02 07:06:34 +0700

    Fixed: convert map utils

--- lib/core/utility/map_cast_converter.dart
+++ lib/core/utility/map_cast_converter.dart
@@ -3,7 +3,15 @@ Map<String, dynamic> convertMap(Map<dynamic, dynamic> map) {
   return <String, dynamic>{
     ...{
       for (final key in map.keys)
-        if (map[key] is Map) key: convertMap(map[key]) else key: map[key],
+        if (map[key] is Map)
+          key: convertMap(map[key])
+        else if (map[key] is List<dynamic>)
+          key: [
+            for (final data in map[key])
+              if (data is Map) convertMap(data) else data
+          ]
+        else
+          key: map[key],
     }
   };
 }
